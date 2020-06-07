9ebe841b1e355a58573482cc484ad7fbe95854fd
Germán López Pineda <me1lopig@uco.es>
2020-06-07 14:11:43 +0200

    Cambios POO

---
+++ moverObjeto/mover.h
@@ -0,0 +1,23 @@
+#pragma once
+class mover
+{
+private:
+	
+	float x;
+	float y;
+
+	mover()
+	{
+		// inicializacion de las coordenadas iniciales
+		x = 0;
+		y = 0;
+	}
+
+
+public:
+
+
+
+
+};
+
---
+++ moverObjeto/moverObjeto.cpp
@@ -0,0 +1,25 @@
+// moverObjeto.cpp : mover objeto en un tablero
+//
+
+#include <iostream>
+#include "mover.h"
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+
+
+
+    cout << "Pulsa una tecla para terminar" << endl;
+    cin.get();
+
+
+    return 0;
+
+
+}
+
+
